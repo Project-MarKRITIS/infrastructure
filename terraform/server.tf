2aed896cc3e3b8a881c5a7ceded42a6a349e3d01
krantzy <ykrantz.dev@proton.me>
2026-09-13 10:10:46 +0200

    fix: firewall reference in server.tf

--- terraform/server.tf
+++ terraform/server.tf
@@ -1,10 +1,10 @@
 resource "hcloud_server" "main" {
   name = "main-node"
-  server_type = "cx22"
+  server_type = "cx23"
   image = "ubuntu-24.04"
   location = "fsn1"
   ssh_keys = data.hcloud_ssh_keys.all.ssh_keys[*].id
-  firewall_ids = [hcloud_firewall.id]
+  firewall_ids = [hcloud_firewall.main.id]
   network {
     network_id = hcloud_network.main.id
     ip = "10.0.1.10"
