2ccbdfa5845b02cbdecef37ec8fdb4f7925bb17f
krantzy <ykrantz.dev@proton.me>
2026-09-14 14:02:53 +0200

    fix: fixed dns entries in dns.tf

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,18 +2,22 @@ data "hcloud_zone" "main" {
     id = var.DOMAIN_ID
 }
 
-resource "hcloud_dns_record" "wildcard" {
-  zone_id = data.hcloud_zone.main.id
-  name    = "*"
-  type    = "A"
-  value   = hcloud_server.main.ipv4_address
-  ttl     = 300
+resource "hcloud_zone_rrset" "wildcard" {
+    zone = data.hcloud_zone.main.name
+    name = "*"
+    type = "A"
+    ttl = 300
+    records = [{
+        value = hcloud_server.main.ipv4_address
+    }]
 }
 
-resource "hcloud_dns_record" "root" {
-  zone_id = data.hcloud_zone.main.id
-  name    = "@"
-  type    = "A"
-  value   = hcloud_server.main.ipv4_address
-  ttl     = 300
+resource "hcloud_zone_rrset" "root" {
+    zone = data.hcloud_zone.main.name
+    name = "@"
+    type = "A"
+    ttl = 300
+    records = [{
+        value = hcloud_server.main.ipv4_address
+    }]
 }
