725b32f93124b66b89c22d879d5bf52439fdbc76
krantzy <ykrantz.dev@proton.me>
2026-09-19 10:37:49 +0200

    feat: create new volume for persistent storage

---
+++ terraform/volume.tf
@@ -0,0 +1,16 @@
+resource "hcloud_volume" "data" {
+  name      = "k3s-data"
+  size      = 20
+  location  = "fsn1"
+  format    = "ext4"
+}
+
+resource "hcloud_volume_attachment" "data" {
+  volume_id = hcloud_volume.data.id
+  server_id = hcloud_server.main.id
+  automount = false
+}
+
+output "data_volume_device" {
+  value = hcloud_volume.data.linux_device
+}
