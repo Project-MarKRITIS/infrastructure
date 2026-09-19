4c49ac857d75a007f3d3e1737bba3fd6a91d4020
krantzy <ykrantz.dev@proton.me>
2026-09-19 08:14:19 +0200

    chore: remove ssh from firewall to prevent access outside of tailscale network

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -4,13 +4,6 @@ resource "hcloud_firewall" "main" {
   rule {
     direction = "in"
     protocol = "tcp"
-    port = "22"
-    source_ips = ["0.0.0.0/0", "::/0"]
-  }
-
-  rule {
-    direction = "in"
-    protocol = "tcp"
     port = "80"
     source_ips = ["0.0.0.0/0", "::/0"]
   }
